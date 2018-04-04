7517cc0acc454b716b0988513b14319870edde68
Darren Oh <darren@oh.name>
2018-04-04 09:34:02 -0400

    feat: Add history component

--- App.js
+++ App.js
@@ -1,28 +1,19 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {View} from 'react-native';
 import AddEntry from './components/AddEntry';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
+import History from './components/History';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={styles.container}>
-          <AddEntry />
+        <View style={{flex: 1}}>
+          <History />
         </View>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    alignItems: 'stretch',
-    justifyContent: 'center'
-  },
-});
